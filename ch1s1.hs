c47e3fb4ad7f9547111f49035fe47ed566e0c3bf
Chris Taylor <crntaylor@gmail.com>
2012-03-24 01:21:55 +0000

    Ex 11 and 12

--- ch1s1.hs
+++ ch1s1.hs
@@ -1,5 +1,3 @@
-import Prelude hiding (sqrt)
-
 -- 1.1 n/a
 
 -- 1.2
@@ -28,15 +26,6 @@ ex1_3 a b c
 -- 1.6 n/a
 
 -- 1.7
-sqrt :: (Ord a, Floating a) => a -> a
-sqrt x = sqrtIter 1.0 x
-    where
-        sqrtIter guess x = if goodEnough guess
-            then guess
-            else sqrtIter (improve guess) x
-        goodEnough guess = abs (guess * guess - x) < 0.001
-        improve guess = average guess (x / guess)
-
 sqrtRel :: (Ord a, Floating a) => a -> a
 sqrtRel x = sqrtIter 1.0 2.0 x
     where
@@ -59,3 +48,33 @@ cbrt x = cbrtIter 1.0 x
         goodEnough guess = abs (guess * guess * guess / x - 1) < 1e-8
         improve guess = (2 * guess + x / (guess * guess)) / 3
 
+-- 1.9
+add :: (Enum a, Num a) => a -> a -> a
+add 0 b = b
+add a b = succ $ add (pred a) b
+
+add' :: (Enum a, Num a) => a -> a -> a
+add' 0 b = b
+add' a b = add' (pred a) (succ b)
+
+-- 1.10 n/a
+
+-- 1.11
+f :: Integral a => a -> a
+f n = if n < 3
+    then n
+    else f (n-1) + 2 * f (n-2) + 3 * f (n-3)
+
+f' :: Integral a => a -> a
+f' n = fIter 2 1 0 n
+    where
+        fIter _ _ c 0 = c
+        fIter a b c n = fIter (a + 2 * b + 3 * c) a b (n-1)
+
+-- 1.12
+pascal :: Integral a => a -> a -> a
+pascal n k
+    | k == 1 || k == n = 1
+    | otherwise = pascal (n-1) (k-1) + pascal (n-1) k
+
+-- 1.13 n/a
