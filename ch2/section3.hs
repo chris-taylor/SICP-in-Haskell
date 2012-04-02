e6bd2555b44b3d4a1afcec21e72c7b2adc39d81d
Chris Taylor <crntaylor@gmail.com>
2012-04-02 17:15:11 +0200

    Started section 3

---
+++ ch2/section3.hs
@@ -0,0 +1,13 @@
+
+
+-- Section 3
+
+-- 2.54 implement isEqual for lists
+-- 2.55 n/a
+
+-- 2.56 Symbolic differentation setup
+-- handle d/dx u^n = n u ^(n-1) du/dx
+
+-- 2.57 sums/products of arbitrary terms
+
+
